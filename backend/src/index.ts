5982ac4338164888dce24057767f0d99dd71b02d
Matheus Carmo <matheus2018i@gmail.com>
2026-08-01 19:51:29 -0300

    fix: update server listen address to allow external connections

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -52,7 +52,7 @@ app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   });
 });
 
-const server = app.listen(port, '127.0.0.1',  (e) => {
+const server = app.listen(port, '0.0.0.0',  (e) => {
   startScheduler();
   getSocket()
     .then(() => console.log("[Messaging] WhatsApp socket initialized"))
